b447d8b742dd2c07a6372703f531112ff1e579d7
NIRMAL CHHETRI <105311660+nirmalkchhetri07@users.noreply.github.com>
2025-01-11 10:03:25 +0545

    Update script.js

--- script.js
+++ script.js
@@ -14,6 +14,6 @@ function calculate() {
   try {
     display.value = eval(display.value); // Evaluate the expression
   } catch (error) {
-    display.value = "Error"; // Handle invalid expressions
+    display.value = "Math Error"; // Handle invalid expressions
   }
 }
